edaf61a6e2dc9107f144c47dd7ab3b4ab4ce9bfa
ftruzzi <francesco@truzzi.me>
2015-03-25 17:08:31 +0100

    New sounds added.

--- js/script.js
+++ js/script.js
@@ -1,41 +1,26 @@
-var effects = false, loop = true, currentbg = 0, FF = !(window.mozInnerScreenX == null), Safari = navigator.vendor.indexOf("Apple")==0 && /\sSafari\//.test(navigator.userAgent); // true or false
-
-
-function GetIEVersion() {
-  var sAgent = window.navigator.userAgent;
-  var Idx = sAgent.indexOf("MSIE");
-
-  // If IE, return version number.
-  if (Idx > 0) 
-    return parseInt(sAgent.substring(Idx+ 5, sAgent.indexOf(".", Idx)));
-
-  // If IE 11 then look for Updated user agent string.
-  else if (!!navigator.userAgent.match(/Trident\/7\./)) 
-    return 11;
-
-  else
-    return 0; //It is not IE
-}
-
+var alt = false, currentbg = 0, altPressed = false, effects = false, loop = true;
+var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor), isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1; isSafari = navigator.vendor.indexOf("Apple")==0 && /\sSafari\//.test(navigator.userAgent), isAndroid = navigator.userAgent.toLowerCase().indexOf("android") > -1;
 
 var bgmusic = [
 	{file: "bennyhill"},
 	{file: "cancan"},
 	{file: "flag"}
-]
+];
 
 var combos = [
 	{key: 32, file: "SHISH", desc: "SHISH!"}, //spacebar
-	{key: 81, file: "DEDEDEDE", desc: "DE DE DE DE"}, //Q
-	{key: 87, file: "DEDEDEDE2", desc: "DE DE DE DE DE DE"}, //W
-	{key: 69, file: "ehm", desc: "EHMHMHMH"}, //E
+	{key: 81, file: "DEDEDEDE", desc: "DE DE DE DE", alt_file: "english", alt_desc: "ENGLISH MOTHERFUCKER"}, //Q
+	{key: 87, file: "DEDEDEDE2", desc: "DE DE DE DE DE DE", alt_file: "succhi", alt_desc: "TU SUCCHI CAZZI?"}, //W
+	{key: 69, file: "ehm", desc: "EHMHMHMH", alt_file: "soffi", alt_desc: "CI SOFFI DENTRO?"}, //E
+	{key: 82, file: "bicoz", desc: "BICOZ"}, //R
 	{key: 88, file: "GENIUS", desc: "GENIUS!"}, //X
-	{key: 65, file: "eighty", desc: "EIGHTY UUUUH"}, //A
-	{key: 83, file: "mistraccio", desc: "FEEHEHEH MISTRACCIOH"}, //S
+	{key: 65, file: "eighty", desc: "EIGHTY UUUUH", alt_file: "FOX"}, //A
+	{key: 83, file: "mistraccio", desc: "FEEHEHEH MISTRACCIOH", alt_file: "nein"}, //S
 	{key: 68, file: "lanch", desc: "LANCHHEEH"}, //D
 	{key: 79, file:"speaking", desc: "TO SPEAKING ABBOUZHE"}, //O
-	{key: 90, file: "mader", desc: "MAI MADER"}, //Z
+	{key: 90, file: "mader", desc: "MAI MADER", alt_file: "mona"}, //Z
 	{key: 67, file: "CRAI", desc: "CRAI"}, //C
+	{key: 85, file: "destroyed", desc: "DESTROYD"}, //U
 	{key: 73, file: "time", desc: "TIME TO TO TO"}, //I
 	{key: 66, file: "commerciala", desc: "DEIDEA WITAUT MARKET COMMERCIAL-H"}, //B
 	{key: 76, file: "berlin", desc: "BERLIN WALLZ"}, //L
@@ -48,8 +33,8 @@ var combos = [
 	{key: 71, file: bgmusic[currentbg].file, desc:String.fromCharCode(9835)}
 ];
 
-
 $(document).ready(function(){
+	if(isAndroid) window.alert("Please rotate your device to landscape (horizontal) mode.");
 	updateZoom();
 	//create audio elements in HTML file
 	for(i=0;i<combos.length-1;i++){
@@ -58,14 +43,22 @@ $(document).ready(function(){
 		audio.setAttribute('src', "audio/"+combos[i].file+".mp3");
 		document.body.appendChild(audio);
 	}
+	for(i=0;i<combos.length-1;i++){
+		if(combos[i].alt_file!=undefined){
+			var audio = document.createElement("audio");
+			audio.setAttribute('id', combos[i].alt_file);
+			audio.setAttribute('src', "audio/"+combos[i].alt_file+".mp3");
+			document.body.appendChild(audio);
+		}
+	}
 	for(i=0;i<bgmusic.length;i++){
 		var audio = document.createElement("audio");
 		audio.setAttribute('id', bgmusic[i].file);
 		audio.setAttribute('src', "audio/"+bgmusic[i].file+".mp3");
 		document.body.appendChild(audio);
 	}
-	if(!FF && !GetIEVersion() && !Safari) process(71);
-	if(FF || GetIEVersion()){
+	if(isChrome) process(71);
+	if(!isChrome && !isSafari){
 		$(".letter").css({
 			'font-family': 'Verdana'
 		})		
@@ -79,89 +72,121 @@ $(window).resize(function(){
 $(document).keydown(function(e){
 	$("div").removeClass("clicked");
 	e.preventDefault();
-	process(e.which);	
+	process(e.which);
 });
 
-$(".key").click(function(){
-	letter = $(this).find(".letter").html();
-	if(letter=="SHISH!") keycode = 32;
-	else keycode = letter.charCodeAt();
-
-	if(keycode==8678) keycode=70;
-	if(keycode==8680) keycode=72;
-
-	process(keycode);
-
-	if(keycode == 9654 || keycode == 9632){
-		specialToggle();
+$(document).keyup(function(e){
+	
+	if(e.which==18 && alt && altPressed){
+		e.preventDefault();
+		altKeys();
+		$("#recplay").addClass("hidden");
+		altPressed = false;
 	}
 });
 
-function updateZoom(){
-	$(".keyboard").css({
-	'zoom': window.screen.width/1920
-	});
-	$(".header").css({
-	'zoom': window.screen.width/1920
-	});
-}
+$(".key").click(function(){
+	var cont = true;
+	letter = $(this).find(".letter").html();
 
+	switch(letter){
+		case "SHISH!":
+			keycode = 32;
+			break;
 
-function process(key){
-	if(key==70 || key==72){ //prev & next
-		stop(bgmusic[currentbg].file);
-		if($("#playbutt").parent().hasClass("special_toggle")==true){
-			$("#playbutt").parent().removeClass("special_toggle");
-			playbutt.html("&#9654;");
-		}
-		changeMusic(key);
-		if(key==70){ //prev
-			key = $("#prev").parent();
-			key.toggleClass("special_toggle");
-			setTimeout(function(){
-				key.removeClass("special_toggle");
-			}, 100);
-		}
-		if(key==72){ //next
-			key=$("#next").parent();
-			key.toggleClass("special_toggle");
-			setTimeout(function(){
-				key.removeClass("special_toggle");
-			}, 100);
-		} 
-	} else if (key==71) { //play/stop
-		specialToggle();	
-	} else if (key==84){ //T (loop)
-		glow(84);
-		loop = toggleOption(loop, "#loop");
-	}else if (key==89){ //Y (effects)
-		glow(89);
-		effects = toggleOption(effects, "#effects");
-	}else{
-		for(i=0;i<combos.length;i++){
-			if(key == combos[i].key){
-				play(combos[i].file);
-				glow(key);
-				scritte(combos[i].desc);
-				break;
+		case "ALT":
+			if(altPressed){
+				altKeys();
+				altPressed=false;
+				cont = false;
 			}
-		}
+			keycode = 18;
+			break;
+
+		default:
+			keycode = letter.charCodeAt();
+			break;
+	}
+	if(cont) process(keycode);
+});
+
+function altKeys(){
+	$(".alt").toggleClass("special_toggle");
+	$(".message").toggleClass("hidden");
+	$(".message2").toggleClass("hidden");
+
+	if(!alt){
+		$(".key:not(:has(.alt_message))").find(".letter").addClass("out");
+		$(".letter").not(".out").not(".option").addClass("red");
+
+		$("#loop").removeClass("option").addClass("out");
+		$("#effects").removeClass("option").addClass("out");
+
+		$("#prev").html("F").removeClass("special").removeClass("red");
+		
+		$("#playbutt").html("G").removeClass("special").removeClass("red").parent().removeClass("special_toggle");
+
+		$("#next").html("H").removeClass("special").removeClass("red");
+
+		$(".alt").find(".letter").addClass("red");
+
+		alt = true;
+	} else {
+		$(".key:not(:has(.alt_message))").find(".letter").removeClass("out");
+		$(".letter").not(".out").not(".option").not("#effects").removeClass("red");
+
+		$("#prev").html("&#8678;").addClass("special").addClass("red");
+
+		if(document.getElementById(bgmusic[currentbg].file).currentTime!=0){
+			$("#playbutt").html("&#9632;").addClass("special").addClass("red").parent().addClass("special_toggle");
+		} else $("#playbutt").html("&#9654;").addClass("special").addClass("red");
+
+		$("#next").html("&#8680;").addClass("special").addClass("red");
+
+		if(effects) $("#effects").addClass("option");
+		else $("#effects").addClass("out");
+
+		if(loop) $("#loop").addClass("option");
+		else $("#loop").addClass("out");
+
+		$(".alt").find(".letter").removeClass("red");
+
+		alt = false;
 	}
 }
 
 function changeMusic(key){
-	if(key==70){
+	if(key==70 && !altPressed){
 		if(currentbg==0) currentbg=bgmusic.length-1;
 		else currentbg--;
 	}
-	if(key==72){
+	if(key==72 && !altPressed){
 		if(currentbg==bgmusic.length-1) currentbg=0;
 		else currentbg++;
 	}
 	combos[combos.length-1].file=bgmusic[currentbg].file;
 }
 
-function specialToggle(){
+function glow(keypr){
+	if(keypr == 32) key = $("#spacebar");
+	else{
+		keychar = String.fromCharCode(keypr);
+		key = $('.letter').filter(function(){
+			return $(this).text() === keychar;
+		}).parent()
+	};
+	key.toggleClass("clicked");
+	setTimeout(function(){
+		key.removeClass("clicked");
+	}, 100);
+}
+
+function play(id){
+	document.getElementById(id).currentTime=0;
+	$("#"+id).trigger("play");
+}
+
+function playToggle(){
 	playbutt=$("#playbutt");
 	playbutt.parent().toggleClass("special_toggle");
 	if(playbutt.html().charCodeAt()==9654){
@@ -185,40 +210,99 @@ function specialToggle(){
 	}
 }
 
-function toggleOption(value, id){
-	if(value){
-		$(id).removeClass("option").addClass("out");
-		return false
+function process(key){
+	if(!altPressed){
+		switch(key){
+			case 8678:
+			case 70: //prev-recplay
+				stop(bgmusic[currentbg].file);
+				if($("#playbutt").parent().hasClass("special_toggle")){
+					$("#playbutt").parent().removeClass("special_toggle");
+					playbutt.html("&#9654;");
+				}
+				changeMusic(70);
+				playToggle();
+
+				key = $("#prev").parent();
+					key.toggleClass("special_toggle");
+					setTimeout(function(){
+						key.removeClass("special_toggle");
+					}, 100);
+			
+				break;
+
+			case 8680:
+			case 72: //next
+				stop(bgmusic[currentbg].file);
+				if($("#playbutt").parent().hasClass("special_toggle")){
+					$("#playbutt").parent().removeClass("special_toggle");
+					playbutt.html("&#9654;");
+				}
+				changeMusic(72);
+				playToggle();
+
+				key=$("#next").parent();
+				key.toggleClass("special_toggle");
+				setTimeout(function(){
+					key.removeClass("special_toggle");
+				}, 100);
+				
+				break;
+
+			case 9654: //play
+			case 9632: //stop
+			case 71: //play-stop
+				playToggle();
+				break;
+
+			case 84: //T (loop)
+				glow(key);
+				loop = toggleOption(loop, "#loop");
+				break;
+
+			case 89: //Y (effects)
+				glow(key);
+				effects = toggleOption(effects, "#effects");
+				break;
+
+			case 18: //ALT
+				altKeys();
+				altPressed = true;
+				break;
+
+			default:
+				for(i=0;i<combos.length;i++){
+					if(key == combos[i].key){
+						glow(key);
+						play(combos[i].file);
+						scritte(combos[i].desc);
+						break;
+					}
+				}
+		}
+	} else {
+		switch(key){
+			case 8678:
+			case 70: //prev-recplay
+			case 8680:
+			case 72: //next
+			case 9654: //play
+			case 9632: //stop
+			case 71: //play-stop
+			case 84:
+			case 89:
+				break;
+
+			default:
+				for(i=0;i<combos.length;i++){
+					if(key == combos[i].key && combos[i].alt_file!=undefined){
+						glow(key);
+						play(combos[i].alt_file);
+						scritte(combos[i].alt_desc);
+					}
+				}
+		}
 	}
-	else {
-		$(id).addClass("option");
-		return true;
-	}
-}
-
-function play(id){
-	document.getElementById(id).currentTime=0;
-	$("#"+id).trigger("play");
-}
-
-function stop(id){
-	audio = document.getElementById(id);
-	audio.pause();
-	audio.currentTime=0;
-}
-
-function glow(keypr){
-	if(keypr == 32) key = $("#spacebar");
-	else{
-		keychar = String.fromCharCode(keypr);
-		key = $('.letter').filter(function(){
-			return $(this).text() === keychar;
-		}).parent()
-	};
-	key.toggleClass("clicked");
-	setTimeout(function(){
-		key.removeClass("clicked");
-	}, 100);
 }
 
 function scritte(desc){
@@ -268,4 +352,31 @@ function scritte(desc){
 			});
 		}
 	}
+}
+
+function stop(id){
+	audio = document.getElementById(id);
+	audio.pause();
+	audio.currentTime=0;
+}
+
+function toggleOption(value, id){
+	if(value){
+		$(id).removeClass("option").addClass("out");
+		return false
+	}
+	else {
+		$(id).addClass("option");
+		return true;
+	}
+}
+
+function updateZoom(){
+	//ugly css hacks here, will improve the keyboard code
+	$(".keyboard").css({
+	'zoom': window.screen.width/1920
+	});
+	$(".header").css({
+	'zoom': window.screen.width/1920
+	});
 }
